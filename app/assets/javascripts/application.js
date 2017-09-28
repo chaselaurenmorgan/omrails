b69a747fd062000fcee5e904b55f324ffc0df1b4
Chase Morgan <chasemorgan15@yahoo.com>
2017-09-27 22:38:28 -0700

    Re-ordered JavaScript Again

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,7 @@
 // about supported directives.
 //
 //= require jquery3
-//= require rails-ujs
+//= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
-//= require popper
